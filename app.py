c289513a14a9307298ca7456ab71a3e4131d271e
Muhammad Fa-aiz Bin Mohammad Fammi <2002784@sit.singaporetech.edu.sg>
2022-05-20 14:57:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,19 +10,9 @@ def default():
     return render_template('index.html')
 
 
-@app.route('/pastebin')
-def pastebin():
-    return render_template('pastebin.html')
-
-
-@app.route('/twitter')
-def twitter():
-    return render_template('twitter.html')
-
-
-@app.route('/reddit')
-def reddit():
-    return render_template('reddit.html')
+@app.route('/results', methods=['POST'])
+def results():
+    return render_template('results.html')
 
 
 if __name__ == '__main__':
