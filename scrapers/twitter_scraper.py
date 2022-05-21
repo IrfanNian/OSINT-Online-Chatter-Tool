0a238ab25f4daa752e96394a4e3bceebaca6d7d2
takaramirin <2000819@sit.singaporetech.edu.sg>
2022-05-21 17:40:40 +0800

    Update twitter_scraper.py

--- scrapers/twitter_scraper.py
+++ scrapers/twitter_scraper.py
@@ -2,19 +2,34 @@ import snscrape.modules.twitter as sntwitter
 import pandas as pd
 import os
 
-
 CWD = os.getcwd()
 
 
 class TwitterScraper:
-    def run(self, arg_search):
+    def run(self, arg_searchbar, arg_advance_since, arg_advance_until, arg_advance_limit):
+        default_tweets = 200  # temporary
+
         # Creating list to append tweet data to
         tweets_list = []
 
+        # Checking for timeframe
+        if arg_advance_since is not None and arg_advance_until is not None:
+            statement = arg_searchbar + "since:" + arg_advance_since + " until:" + arg_advance_until
+        elif arg_advance_since is None and arg_advance_until is not None:
+            statement = arg_searchbar + "until:" + arg_advance_until
+        elif arg_advance_since is not None and arg_advance_until is None:
+            statement = arg_searchbar + "since:" + arg_advance_since
+        else:
+            statement = arg_searchbar
+
+        # Check for tweet limit
+        if type(arg_advance_limit) == int:
+            default_tweets = arg_advance_limit
+
         # Using TwitterSearchScraper to scrape data and append tweets to list
         for i, tweet in enumerate(
-                sntwitter.TwitterSearchScraper(arg_search).get_items()):
-            if i > 200:
+                sntwitter.TwitterSearchScraper(statement).get_items()):
+            if i > default_tweets:
                 break
             tweets_list.append([tweet.date, tweet.id, tweet.content, tweet.user.username])
 
