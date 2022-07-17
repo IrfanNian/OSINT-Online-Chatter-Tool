4432637b88e1f8b147bf905bfa3c42c23268d2e1
Cherie Teo <teocheries@gmail.com>
2022-07-17 18:15:05 +0800

    remove unnecessary code

--- static/js/result.js
+++ static/js/result.js
@@ -110,13 +110,6 @@ drawBar.addEventListener("click", function () {
     drawBarChart();
 });
 
-let table = document.querySelector("table tbody");
-table.textContent = "";
-row = table.insertRow(0);
-var cell1 = row.insertCell(0);
-var cell2 = row.insertCell(1);
-cell1.textContent = "NOTE:";
-cell2.textContent = "Click on a datapoint to display its contents!";
 
 function drawCloud() {
     d3.csv("/static/results/charting.csv").then(function (datapoints) {
